b0dcfc3308c4468321c0ae085aa97a1c41fd1efd
Nathan <92413483+n8zone@users.noreply.github.com>
2024-11-16 19:25:32 -0500

    Add deletion

--- readme.md
+++ readme.md
@@ -10,18 +10,22 @@ A simple command line tool to add convenient markers to jump around directories
 The following function must be added to your bash configuration (.bashrc / .zshrc)
 
 ~/.local/bin/tp.py can be changed to wherever you wish to keep the script.
-```
+
+```bash
 function tp() {
-	if [ "$1" = "--add" ] || [ "$1" = "-a" ]; then
-		python3 ~/.local/bin/tp.py "$@"
-	else
-		dir=$(python3 ~/.local/bin/tp.py "$1")
-		if [ -d "$dir" ]; then
-			cd "$dir"
-		else
-			echo "Error: $dir"
-		fi
-	fi
+    case "$1" in
+        -d|--delete|-a|--add|-l|--list)
+            python3 ~/.local/bin/tp.py "$@"
+            ;;
+        *)
+            dir=$(python3 ~/.local/bin/tp.py "$1")
+            if [ -d "$dir" ]; then
+                cd "$dir"
+            else
+                echo "Error: $dir"
+            fi
+            ;;
+    esac
 }
 ```
 
@@ -32,6 +36,8 @@ To add a new marker run the `tp` command with the `-a` or `--add` flag and then 
 To jump to a marker simply use `tp marker_name`
 
 To list all current markers use `tp -l` or `tp --list`
+
+To delete a marker use `tp -d marker_name` or `tp --delete marker_name`
 
 
 ## Important Notes
--- tp.py
+++ tp.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description="Move quickly between directories."
 
 parser.add_argument('-a', '--add', help='Adds a new marker by the provided name', action='store_true')
 parser.add_argument('-l', '--list', help='Lists all existing markers', action='store_true')
+parser.add_argument('-d', '--delete', help='Deletes target marker', action='store_true')
 parser.add_argument("name", nargs='?', help='The name of the marker to jump or modify')
 
 args = parser.parse_args()
@@ -43,11 +44,30 @@ def add_to_dict(marker_name: str, path: str):
         os.makedirs(os.path.join(os.environ["HOME"], "term-tp"))
         add_to_dict(marker_name, path)
 
+
+# I think explicitly typing dicts is good, but I don't like it anywhere else tbh
+def delete_from_dict(marker_name: str, destinations: dict [str, str]):
+    try:
+        dest_file = open(f'{destinations_dir}/testfail.txt', 'w')
+        for marker in destinations:
+            path = destinations[marker]
+            dest_file.write(f'{marker}:{path};')
+        print(f"Marker {marker_name} removed!")
+    except FileNotFoundError:
+        os.makedirs(os.path.join(os.environ["HOME"], "term-tp"))
+        delete_from_dict(marker_name, destinations)
+
 # Read and initialize destinations
 dest_file = None
 def main():
     destinations = load_dict()
-    if args.list:
+    if args.delete:
+        target = args.name
+        print(f"Deleting: {target}")
+        if target in destinations:
+            del destinations[target]
+            delete_from_dict(target, destinations)
+    elif args.list:
         print(f"\n{'MARKER':20}:\tDESTINATION")
         for marker in destinations:
             print(f"{marker:20}:\t{destinations[marker]}")
