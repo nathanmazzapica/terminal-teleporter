d9190fc28d9f936933ebee5f557b1c7ed0584505
Nathan <92413483+n8zone@users.noreply.github.com>
2024-11-19 09:16:27 -0500

    General code cleanup

--- tp.py
+++ tp.py
@@ -2,84 +2,75 @@ import sys
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description="Move quickly between directories.")
 
-parser.add_argument('-a', '--add', help='Adds a new marker by the provided name', action='store_true')
-parser.add_argument('-l', '--list', help='Lists all existing markers', action='store_true')
-parser.add_argument('-d', '--delete', help='Deletes target marker', action='store_true')
-parser.add_argument("name", nargs='?', help='The name of the marker to jump or modify')
+MARKERS_DIR = os.path.join(os.environ["HOME"], "term-tp")
+MARKERS_FILE = os.path.join(MARKERS_DIR, "markers.txt")
 
-args = parser.parse_args()
+def ensure_markers_dir_exists():
+    os.makedirs(MARKERS_DIR, exist_ok=True)
 
-destinations_dir = os.path.join(os.environ["HOME"], "term-tp")
 
-def load_dict() -> dict[str, str]:
-    destinations: dict[str, str] = {}
-    try:
-        dest_file = open(f'{destinations_dir}/markers.txt', 'r')
-        pairs = dest_file.read()
-        for pair in pairs.split(";"):
-            if len(pair) == 0:
-                continue
-
-            target = pair.split(":")
-            marker = target[0]
-            target_dir = target[1]
-            destinations[marker] = target_dir
-
-    except FileNotFoundError:
-        if not args.add:
-            print("You have not created any markers!")
-            print("Please use this command with the -a/--add flag to create one!")
+def load_markers() -> dict[str, str]:
+    if not os.path.exists(MARKERS_FILE):
         return {}
+    with open(MARKERS_FILE, 'r') as file:
+        return {k:v for k, v in (line.split(":") for line in file.read().split(";") if line)}
 
-    return destinations
-
-def add_to_dict(marker_name: str, path: str):
-    try:
-        dest_file = open(f'{destinations_dir}/markers.txt', 'a')
-        dest_file.write(f'{marker_name}:{path};')
-        print(f"Marker {marker_name} placed at {os.getcwd()}!")
-    except FileNotFoundError:
-        os.makedirs(os.path.join(os.environ["HOME"], "term-tp"))
-        add_to_dict(marker_name, path)
+def save_markers(markers: dict[str, str]):
+    ensure_markers_dir_exists()
+    with open(MARKERS_FILE, 'w') as file:
+        file.write(";".join(f"{k}:{v}" for k, v in markers.items()))
 
 
-# I think explicitly typing dicts is good, but I don't like it anywhere else tbh
-def delete_from_dict(marker_name: str, destinations: dict [str, str]):
-    try:
-        dest_file = open(f'{destinations_dir}/markers.txt', 'w')
-        for marker in destinations:
-            path = destinations[marker]
-            dest_file.write(f'{marker}:{path};')
-        print(f"Marker {marker_name} removed!")
-    except FileNotFoundError:
-        os.makedirs(os.path.join(os.environ["HOME"], "term-tp"))
-        delete_from_dict(marker_name, destinations)
-
-# Read and initialize destinations
-dest_file = None
 def main():
-    destinations = load_dict()
+    parser = argparse.ArgumentParser(description="Move quickly between directories.")
+    parser.add_argument('-a', '--add', help='Adds a new marker by the provided name', action='store_true')
+    parser.add_argument('-l', '--list', help='Lists all existing markers', action='store_true')
+    parser.add_argument('-d', '--delete', help='Deletes target marker', action='store_true')
+    parser.add_argument("name", nargs='?', help='The name of the marker to jump or modify')
+    args = parser.parse_args()
+
+    markers = load_markers()
+
     if args.delete:
         target = args.name
         print(f"Deleting: {target}")
-        if target in destinations:
-            del destinations[target]
-            delete_from_dict(target, destinations)
+        if target not in markers:
+            print(f"Marker '{args.name}' does not exist!")
+            sys.exit(1)
+        del markers[args.name]
+        save_markers(markers)
+        print(f"Marker '{args.name}' removed!")
+
+    elif args.add:
+        if args.name in markers:
+            print(f"Marker '{args.name}' already exists!")
+            sys.exit(1)
+
+        if args.name is None:
+            print(f"Marker name cannot be blank!")
+            sys.exit(1)
+
+        elif ":" in args.name or ";" in args.name:
+            print(f"{args.name} contains illegal characters!")
+            sys.exit(1)
+
+        markers[args.name] = os.getcwd()
+        save_markers(markers)
+        print(f"Marker '{args.name}' added at {os.getcwd()}!")
+
     elif args.list:
         print(f"\n{'MARKER':20}:\tDESTINATION")
-        for marker in destinations:
-            print(f"{marker:20}:\t{destinations[marker]}")
+        for marker in markers:
+            print(f"{marker:20}:\t{markers[marker]}")
         print("\n")
-    elif args.add:
-        cwd = os.getcwd()
-        marker_name = args.name
-        add_to_dict(marker_name, cwd)
+
     else:
-        if args.name in destinations:
-            print(destinations[args.name])
+        if args.name in markers:
+            print(markers[args.name])
         else:
+            if len(markers) == 0:
+                print(f"You have no created any markers! Use -a or --add to create a marker!")
             print(f"Marker: {args.name} does not exist!")
             sys.exit(1)
 
